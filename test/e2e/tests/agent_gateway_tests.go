e427240064ec6a90fab0188549d6c05ceb200510
Chris Kaiser <58376705+chris-kaiser-7@users.noreply.github.com>
2025-12-08 08:21:40 +0000

    moved inference_extension into agentgateway

--- test/e2e/tests/agent_gateway_tests.go
+++ test/e2e/tests/agent_gateway_tests.go
@@ -12,6 +12,7 @@ import (
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/configmap"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/csrf"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/extauth"
+	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/inference"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/jwtauth"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/mcp"
 	global_rate_limit "github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/rate_limit/global"
@@ -38,6 +39,7 @@ func AgentgatewaySuiteRunner() e2e.SuiteRunner {
 	agentgatewaySuiteRunner.Register("BasicAuth", basicauth.NewTestingSuite)
 	agentgatewaySuiteRunner.Register("ApiKeyAuth", apikeyauth.NewTestingSuite)
 	agentgatewaySuiteRunner.Register("JwtAuth", jwtauth.NewTestingSuite)
+	agentgatewaySuiteRunner.Register("Inference", inference.NewTestingSuite)
 
 	return agentgatewaySuiteRunner
 }
